197fc117bfeb31f4c2930d089700636b09243f4e
RandomNumberSequence <67464857+RandomNumberSequence@users.noreply.github.com>
2023-05-04 13:07:41 +0200

    added Tile classes and refactored Arena
    refactored Arena to take a more object oriented approach

--- robo-arena.py
+++ robo-arena.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import sys
+import tiles
 
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
@@ -46,13 +47,23 @@ class Arena(QFrame):
 
     def initArena(self):
         # create an arena layout with a matrix filled at the start only with "normal" tiles
-        self.ArenaLayout = [[random.randint(0,5) for j in range(Arena.ArenaWidth)] for i in range(Arena.ArenaHeight)]
-
-    # generates a random layout by filling the matrix ArenaLayout with random integers which represent the diffrent tiles
-    #def setRandomLayout(self):
-    #    for i in range(Arena.ArenaHeight):
-    #        for j in range(Arena.ArenaWidth):
-    #            self.ArenaLayout[i][j] = random.randint(0,5)
+         self.ArenaLayout = [[self.randomTile() for j in range(self.ArenaWidth)] for i in range(self.ArenaHeight)]
+    
+    def randomTile(self):
+        h = random.randint(0,5)
+        if h == 0:
+            tile = tiles.NormalTile()
+        elif h == 1:
+            tile = tiles.WallTile()
+        elif h == 2:
+            tile = tiles.FireTile()
+        elif h == 3:
+            tile = tiles.IceTile()
+        elif h == 4:
+            tile = tiles.WaterTile()
+        elif h == 5:
+            tile = tiles.SandTile()
+        return tile
 
     # paint all tiles of the arena
     def paintEvent(self, event):
@@ -73,23 +84,8 @@ class Arena(QFrame):
 
     # paint a single tile
     def drawTile(self, painter, x, y, tile):
+        painter.fillRect(x, y, tile.width, tile.height, tile.color)
 
-        # color of the tiles
-        colorTable = [0x66CD00, 0x000000, 0xFF4500, 0xB0E2FF,
-                      0x1E90FF, 0xFFEC8B]
-
-        color = QColor(colorTable[tile])
-        painter.fillRect(x, y, Arena.TileWidth,
-                         Arena.TileHeight, color)
-
-# class TileTypes contains the names of all possible tiles 
-class TileTypes(object):
-    Normal = 0
-    Wall   = 1
-    Fire   = 2
-    Ice    = 3
-    Water  = 4
-    Sand   = 5
 
 def main():
 
