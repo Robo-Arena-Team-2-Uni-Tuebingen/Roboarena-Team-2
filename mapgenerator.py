b609af259e0f822aab1de8630231430f56caf69c
RandomNumberSequence <67464857+RandomNumberSequence@users.noreply.github.com>
2023-05-16 10:06:52 +0200

    minor changes

--- mapgenerator.py
+++ mapgenerator.py
@@ -8,22 +8,23 @@ from noise import snoise2, _simplex
 def generateMap(height, width):
     randomX = random()
     randomY = random()
-    factor = 1000
-    arenaLayout = np.matrix([[chooseTile(snoise2(i + (randomX * factor), j + (randomY * factor))) for i in range(width)] for j in range(height)])
+    randomScale = random()
+    factor = 100
+    arenaLayout = np.matrix([[chooseTile(snoise2(randomScale*(i + (randomX * factor)), randomScale*(j + (randomY * factor)))) for i in range(width)] for j in range(height)])
     return arenaLayout
 
 #chooses a tile based on a given float value, impassable tiles should get a lower range to decrease
 #the probability of them blocking the player in
 def chooseTile(f):
-    if f >= -1 and f < -0.5:
+    if f >= -1 and f < -0.7:
         return tiles.FireTile()
-    elif f >= -0.5 and f < -0.2:
+    elif f >= -0.7 and f < -0.2:
         return tiles.SandTile()
     elif f >= -0.2 and f < 0:
         return tiles.NormalTile()
     elif f >= 0 and f < 0.2:
         return tiles.WaterTile()
-    elif f >= 0.2 and f < 0.5:
+    elif f >= 0.2 and f < 0.7:
         return tiles.IceTile()
-    elif f >= 0.5 and f <= 1:
+    elif f >= 0.7 and f <= 1:
         return tiles.WallTile()
