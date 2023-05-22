c36f271288eaafc1a091c4b0e09fe54fbfa02a13
Niklas Wolf <nikwo33@gmail.com>
2023-05-22 10:54:54 +0200

    extended the robot class with speed parameters

--- robot.py
+++ robot.py
@@ -4,18 +4,38 @@ class Robot():
 
     #the size of the robot in px
     radius = 30
+    #maximum accelerations
+    a           = 0
+    a_alpha     = 0
+    A_max       = 100
+    A_alpha_max = 100
+    v           = 0
+    v_alpha     = 0
 
-    def __init__(self, xpos, ypos, alpha):
+    def __init__(self, xpos, ypos, alpha, color):
         
         self.xpos = xpos
         self.ypos = ypos
         #angle the robot in degrees
         self.alpha = alpha - 180
+        self.color = color 
 
     #updates the position of the robot
-    def update(newxpos, newypos):
-        xpos = newxpos
-        ypos = newypos
+    def update(self, newxpos, newypos, new_a, new_a_alpha, t):
+        self.xpos = newxpos
+        self.ypos = newypos
+        self.a     = self.a + new_a
+
+        if self.a >= self.A_max:
+            self.a = self.A_max
+
+        self.a_alpha = self.a_alpha + new_a_alpha
+
+        if self.a_alpha >= self.A_alpha_max:
+            self.a_alpha = self.A_alpha_max
+            
+        self.v       = self.v + self.a * t
+        self.v_alpha = self.v_alpha + self.a_alpha * t
 
     #this function is supposed to get the angle from the robot to a specific point relative to the x-axis
     def getAlpha(self, x, y):
